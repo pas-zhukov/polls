4d527b1179ea83bf975c8871c4455dfcc9faa914
pas-zhukov <pas-zhukov@yandex.ru>
2024-10-03 23:03:58 +0300

    почистил мастер

--- src/main/java/ru/pas_zhukov/models/PollFillingData.java
+++
@@ -1,8 +0,0 @@
-package ru.pas_zhukov.models;
-
-import java.util.List;
-
-public class PollFillingData {
-    private String username;
-    private List<PollQuestionResponse> responses;
-}
--- src/main/java/ru/pas_zhukov/models/PollQuestionResponse.java
+++
@@ -1,8 +0,0 @@
-package ru.pas_zhukov.models;
-
-import java.util.List;
-
-public class PollQuestionResponse {
-    private PollQuestion pollQuestion;
-    List<String> selectedVariants;
-}
