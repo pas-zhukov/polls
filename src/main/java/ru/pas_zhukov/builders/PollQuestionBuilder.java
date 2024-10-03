68fdd77de3a6d510ef7c4bda53169a9af01cfc3e
pas-zhukov <pas-zhukov@yandex.ru>
2024-10-03 23:06:30 +0300

    Revert "почистил мастер"
    This reverts commit 4d527b1179ea83bf975c8871c4455dfcc9faa914.

---
+++ src/main/java/ru/pas_zhukov/builders/PollQuestionBuilder.java
@@ -0,0 +1,23 @@
+package ru.pas_zhukov.builders;
+
+public class PollQuestionBuilder {
+    PollQuestionBuilder withTitle(String title) {
+        return null;
+    }
+
+    PollQuestionBuilder withMinAnswers(int minAnswers) {
+        return null;
+    }
+
+    PollQuestionBuilder withMaxAnswers(int maxAnswers) {
+        return null;
+    }
+
+    PollQuestionBuilder withAnswerVariant(String answerVariant) {
+        return null;
+    }
+
+    PollBuilder and() {
+        return null;
+    }
+}
---
+++ src/main/java/ru/pas_zhukov/models/PollQuestion.java
@@ -0,0 +1,10 @@
+package ru.pas_zhukov.models;
+
+import java.util.List;
+
+public class PollQuestion {
+    private String title;
+    private Integer minAnswers;
+    private Integer maxAnswers;
+    private List<String> answers;
+}
