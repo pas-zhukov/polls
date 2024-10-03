dfa09f90b57f5ebb11031e2568d960ae40fa1c1c
pas-zhukov <pas-zhukov@yandex.ru>
2024-10-03 23:01:45 +0300

    реализация билдера

--- src/main/java/ru/pas_zhukov/builders/PollBuilder.java
+++ src/main/java/ru/pas_zhukov/builders/PollBuilder.java
@@ -1,25 +1,49 @@
 package ru.pas_zhukov.builders;
 
 import ru.pas_zhukov.models.Poll;
+import ru.pas_zhukov.models.PollQuestion;
+
+import java.util.ArrayList;
 
 public class PollBuilder {
+    private Poll poll;
+
+    public PollBuilder() {
+        poll = new Poll("Unnamed poll", new ArrayList<>());
+    }
+
     public PollBuilder withPollName(String pollName) {
-        return null;
+        poll.setPollName(pollName);
+        return this;
     }
 
     public PollQuestionBuilder pollQuestion(String title) {
-        return null;
+        return PollQuestion.builder(this).withTitle(title);
     }
 
     public PollQuestionBuilder oneVariantPollQuestion(String title) {
-        return null;
+        return PollQuestion.builder(this)
+                .withTitle(title)
+                .withMinAnswers(1)
+                .withMaxAnswers(1);
     }
 
     public PollBuilder yesNoPollQuestion(String title) {
-        return null;
+        return PollQuestion.builder(this)
+                .withTitle(title)
+                .withAnswerVariant("Yes")
+                .withAnswerVariant("No")
+                .withMinAnswers(1)
+                .withMaxAnswers(1)
+                .and();
+    }
+
+    public PollBuilder addQuestion(PollQuestion question) {
+        poll.addQuestion(question);
+        return this;
     }
 
     public Poll build() {
-        return null;
+        return poll;
     }
 }
